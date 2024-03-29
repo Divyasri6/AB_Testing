547ad30e5a2f73b37426c53df4d9fa1690a87e81
Divyasri6 <divyasrivemula441@gmail.com>
2024-03-28 22:49:49 -0500

    initial

---
+++ app.py
@@ -0,0 +1,225 @@
+import streamlit as st
+import pandas as pd
+from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
+import pymongo
+
+# -------------- SETTINGS --------------
+MONGO_URI="your uri"
+DATABASE_NAME  = "experiment"
+PROGRAM_OPTIONS = ("Email", "Home phone", "Mobile phone")
+LEVER_OPTIONS = ("timing", "phone", "ll")
+ATTRIBUTE_OPTIONS = ('Green', 'Yellow', 'Red', 'Blue')
+
+LEVEL_EXTRA = ['intervention', 'cohort']
+# -------------------------
+
+@st.cache_resource
+def init_connection():
+    # Connects to the MongoDB database and handles potential connection errors.
+    try:
+        client=pymongo.MongoClient(MONGO_URI)
+        db = client[DATABASE_NAME]
+        print("Connected to MongoDB successfully!")
+    except Exception as e:
+        print(f"Error connecting to MongoDB: {str(e)}")
+        st.stop()  # Halt execution if connection fails
+    return db
+
+
+def save_experiment_data(data, collection):
+    # Saves experiment data to the specified MongoDB collection
+    try:
+        collection.insert_one(data)
+        st.success("Experiment data saved successfully!")
+    except Exception as e:
+        st.error(f"An error occurred while saving data: {str(e)}")
+
+def drop_collection(db, collection_name):
+    # Drops a collection from the MongoDB database.
+    try:
+        db.drop_collection(collection_name)
+        st.success(f"Collection '{collection_name}' dropped successfully!")
+    except Exception as e:
+        st.error(f"Error dropping collection: {str(e)}")
+
+def generate_unique_id(program, lever,db):
+  # Generates a unique TableName for an experiment and create collection for mongoDB.
+      
+    version_id = 1  # Start with version 1 (or adjust based on versioning logic)
+    existing_id = f"{program}_{lever}_V{version_id}"
+    collist = db.list_collection_names()
+    while existing_id in collist:
+        version_id += 1  # Increment version if conflict found
+        existing_id = f"{program}_{lever}_V{version_id}"
+    collection = db[existing_id]
+    return existing_id,collection
+# Initialize session state
+if "experiment_data" not in st.session_state:
+     st.session_state["experiment_data"] = {}
+
+if "row_data" not in st.session_state:
+            st.session_state["row_data"] = []
+
+def add_row_data():
+    if "row_data" not in st.session_state:
+        st.session_state["row_data"] = []
+    st.session_state["row_data"].append({"levels": [f"{item}_{len(st.session_state['row_data']) + 1}" for item in LEVEL_EXTRA]})
+
+
+# -------------------------
+def main():
+    st.set_page_config(page_title="Experimentation", layout="centered")
+    st.title("Experimentation")
+    # Hide Streamlit branding
+    hide_st_style = """
+              <style>
+              #MainMenu {visibility: hidden;}
+              footer {visibility: hidden;}
+              header {visibility: hidden;}
+              </style>
+              """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+
+    # --- NAVIGATION MENU ---
+    selected = option_menu(
+        menu_title=None,
+        options=["create new Experiment", "List and Test Experiment"],
+        icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
+        orientation="horizontal",
+    )
+    db = init_connection()  
+
+    if selected == "create new Experiment":
+        st.header("Data Entry")
+
+        program = st.selectbox(
+            "Program",
+            PROGRAM_OPTIONS,
+            index=None,
+            placeholder="Select program...", key="program"
+        )
+        if program is None:
+                st.error("Please select a program.")
+        lever = st.selectbox(
+            "Lever",
+            LEVER_OPTIONS,
+            index=None,
+            placeholder="Select lever...", key="lever"
+        )
+        if lever is None:
+                st.error("Please select a lever.")
+        attributes = st.multiselect('Attributes', ATTRIBUTE_OPTIONS,placeholder="Select list of attributes...", key="attributes")
+
+       
+        stages = st.number_input("Insert a number", value=1, step=1, key="levels")
+        c1 = [f"{item}_{i + 1}" for i in range(int(stages)) for item in LEVEL_EXTRA]
+
+        Table_name,collection = generate_unique_id(program, lever,db)
+        
+        "---"
+        add_row_button = st.button("Add Experiment")
+        if add_row_button:
+            add_row_data()
+        
+        for i in enumerate(st.session_state["row_data"]):
+            num = i[0] + 1
+            st.subheader(f"Row: {num}")
+                
+            with st.expander("List of Attributes"):
+                level_attr = {attr: st.text_input(f"{attr}:", key=f"attribute_{attr}_{i}") for attr in attributes}
+
+            with st.expander("Levels"):
+                level_stage = {stage: st.text_input(f"{stage}:", key=f"attribute_{stage}_{i}") for stage in c1}
+
+            with st.expander("Lever"):
+                timing = st.text_input(f"{lever}", key=f"{lever}_{i}")
+                champion_lever = st.text_input(f"Champion_{lever}", key=f"champion_{lever}_{i}")
+                start_date = st.date_input("Start_Date", key=f"start_date_{i}")
+                end_date = st.date_input("End_Date", key=f"end_date_{i}")
+               
+            if Table_name not in st.session_state["experiment_data"]:
+                    st.session_state["experiment_data"][Table_name] = {}
+            data = {
+                    "_id":f"{Table_name}_{i[0]}",
+                    f"{lever}":timing,
+                    f"Champion_{lever}": champion_lever,
+                    "Attributes": level_attr,
+                    "Levels": level_stage,
+                    "Start_date": str(start_date),
+                    "End_date": str(end_date)
+                }
+            st.session_state["experiment_data"][Table_name][f"{Table_name}_{i[0]}"] = data 
+        
+        if st.session_state["row_data"]:
+            review_button = st.button("Review Data")
+            if review_button:
+                st.subheader("Review Data:")
+                for key, data in st.session_state["experiment_data"].items():
+                    st.subheader(f"{key}:")
+                    st.write(data)
+                
+            if st.button("Submit"):
+                try:
+                    for key, value in st.session_state["experiment_data"][Table_name].items():
+                        save_experiment_data(value, collection)
+                except Exception as e:
+                    st.error(f"An error occurred while saving data: {str(e)}")
+                
+    elif selected == "List and Test Experiment":
+        tab1, tab2 = st.tabs(["List Experiment", "Test Experiment"])
+        collist = db.list_collection_names()
+        with tab1:
+            st.header("Existing Experiments",divider='rainbow')
+            # Display existing tables if any
+            if collist:
+                data = []
+                for key in collist:
+                    program, lever, version_id = key.split("_")  # Assuming key format "program_lever_version_id"
+                    data.append({"Program": program, "Lever": lever, "Version ID": version_id})
+
+                # Create a Pandas DataFrame from the data
+                df = pd.DataFrame(data)
+
+                # Display the DataFrame as a table
+                st.table(df)
+                collection_to_drop = st.selectbox("Enter table name to drop:",collist, key="collection_to_drop",index=None,)
+                if st.button("Delete experiment"):
+                    if collection_to_drop:
+                        drop_collection(db, collection_to_drop)
+                    else:
+                        st.error("Please enter a collection name.")
+            else:
+                st.write("No existing experimentation data.")
+        with tab2:
+            with st.form("Test data"):
+                st.header("Test Experiments",divider='rainbow')
+                selected_collection = st.selectbox("Enter experiment name to Test:",collist, key="collection_to_Test",index=None,placeholder="program_lever_version")
+                
+                if st.form_submit_button("generate value"):
+                    Patient_id=st.text_input("Enter patient id")
+                    Test_collection = db[selected_collection]
+                    first_doc =Test_collection.find_one()
+                    attributes_object = first_doc.get("Attributes", {})
+
+                    # Display only the keys of the attributes object
+                    keys = list(attributes_object.keys()) 
+                    for key in keys:
+                        st.text_input(key, key=f"input_{key}")
+                    
+                    with st.spinner("please wait while processing.."):
+                        entered_values = {key: st.session_state[f"input_{key}"] for key in keys}
+                    
+                        # Query to filter documents where the entered attribute values match
+                        query = {"Attributes": entered_values}
+                    
+                        matched_docs = Test_collection.find(query)
+                        if matched_docs:
+                            st.subheader("Test created:")
+                            st.subheader(f"patient id : {Patient_id}" )
+                            for doc in matched_docs:
+                                st.write(doc)                        
+                        else:
+                            st.write("No documents match the entered attribute values.")                                       
+
+if __name__ == '__main__':
+    main()
